3f6d3be5c89c02606d6e46631044700f3d3d283b
Dhairya Gabhawala <dhairyagabha@users.noreply.github.com>
2017-11-04 16:31:53 -0500

    Revert "Merge branch 'master' into dev"
    This reverts commit a425d0634d463bf1fa659c3ab1a2c82eec4c0704.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
   mount Ckeditor::Engine => '/ckeditor'
+  root to: 'home#index'
   devise_for :users
   resources :articles do
     collection do
