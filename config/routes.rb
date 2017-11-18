6dd09f64e0574f25293feb6ab778683ebaa055b7
Dhairya Gabhawala <gabhawaladhairya@gmail.com>
2017-11-18 09:20:02 -0600

    Tag Filters Update and link fixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     end
   end
   resources :comments
+  resources :tags
   get '/admin' => 'admin#index'
   get '/my-articles' => 'admin#my_articles'
   post '/follow-me' => 'admin#follow_user'
